71e62d68b5d4640872219da70b74c47c0302326b
6116974 <166622890+6116974@users.noreply.github.com>
2024-04-11 10:46:38 -0400

    added tool

--- helpers.js
+++ helpers.js
@@ -1,14 +1,32 @@
 function pairDecode(iQuestion) {
+  var iSBinder = Math.ceil(iQuestion / 600);
+  var iBinder = Math.ceil((iQuestion - ((iSBinder-1)*600)) / 25);
+  var iPair = Math.ceil(iQuestion - (iSBinder-1)*600 - (iBinder-1)*25);
+
   var ret = new Object();
-  
-  ret["s_binder"] = Math.floor(iQuestion / 600)+1;
-    
-  ret["binder"] = Math.floor(iQuestion / 25)+1;
-  ret["binder_tip"] = Math.ceil(ret["binder"]/5);
-  ret["binder_ring"] = ret["binder"] - (Math.ceil(ret["binder"]/5)*5-5);
-    
-  ret["pair"] = iQuestion - ((ret["binder"]-1) * 25);
-  ret["pair_tip"] = Math.ceil(ret["pair"]/5);
-  ret["pair_ring"] = ret["pair"] - (Math.ceil(ret["pair"]/5)*5-5);
+  ret["question"] = iQuestion;
+  ret["sbinder"] = iSBinder;
+  ret["binder"] = iBinder;
+  ret["pair"] = iPair;
   return ret;
+}
+
+function getTipRing(iQuestion) {
+  var ret = new Object();
+  ret["question"] = iQuestion;
+  ret["tip"] = Math.ceil(iQuestion/5);
+  ret["ring"] = iQuestion - (ret["tip"]-1)*5;
+  return ret;
+}
+
+function clrStripe(tip, ring) {
+  return 'repeating-linear-gradient(-45deg, '+clrTip(tip)+' 0px, '+clrTip(tip)+' 10px, '+clrRing(ring)+' 10px, '+clrRing(ring)+' 15px)';
+}
+function clrRing(ring) {
+  const colRing = ["#00F","#FA1","#0F0","#B73","#AAA"];
+  return colRing[ring-1];
+}
+function clrTip(tip) {
+  const colTip = ["#FFF","#F00","#000","#FF0","#F0F"];
+  return colTip[tip-1];
 }
